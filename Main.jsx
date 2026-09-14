0ba365188c53cf34d2a06d0d10a739d944c1b3ba
thanmai <thanmaireddy77@gmail.com>
2026-09-14 20:17:56 +0530

    using formData action capabilities to handle form data

--- Main.jsx
+++ Main.jsx
@@ -1,23 +1,22 @@
+import React from "react"
+
 export default function Main() {
-    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
-    
+
+    const [ingredients, setIngredients] = React.useState([])
+
     const ingredientsListItems = ingredients.map(ingredient => (
         <li key={ingredient}>{ingredient}</li>
     ))
-    
 
-    function handleSubmit(event) {
-        event.preventDefault()
-        const formData = new FormData(event.currentTarget)
+    function addIngredient(formData) {
         const newIngredient = formData.get("ingredient")
-        ingredients.push(newIngredient)
-        console.log(ingredients)
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
     }
-    
+
     return (
         <main>
-            <form onSubmit={handleSubmit} className="add-ingredient-form">
-                <input 
+            <form action={addIngredient} className="add-ingredient-form">
+                <input
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
